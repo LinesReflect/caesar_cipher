583597f51c4e7df1707c129cedb8799528387d7f
Reflection <reflectionoflines@gmail.com>
2023-04-12 11:30:52 -0500

    Create the CaesarCipher class.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,20 +1,22 @@
-def caesar_cipher (str, shift)
-    str_words = str.split
-    new_str = []
-    str_words.each do |word|
-      new_word = ""
-      word.each_char do |c|
-        if ("a".."z").include?(c)
-          new_c = ((c.ord - 97 + shift) % 26 + 97).chr
-          new_word += new_c
-        elsif ("A".."Z").include?(c)
-          new_c =  ((c.ord - 65 + shift) % 26 + 65).chr
-          new_word += new_c
-        else
-          new_word += c
+class CaesarCipher
+  def caesar_cipher (str, shift)
+      str_words = str.split
+      new_str = []
+      str_words.each do |word|
+        new_word = ""
+        word.each_char do |c|
+          if ("a".."z").include?(c)
+            new_c = ((c.ord - 97 + shift) % 26 + 97).chr
+            new_word += new_c
+          elsif ("A".."Z").include?(c)
+            new_c =  ((c.ord - 65 + shift) % 26 + 65).chr
+            new_word += new_c
+          else
+            new_word += c
+          end
         end
+        new_str.push(new_word)
       end
-      new_str.push(new_word)
-    end
-    puts new_str.join(" ")
+      new_str.join(" ")
+  end
 end
